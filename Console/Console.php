4bb39658539b53eaa26e04e2d4afe5994ab2277a
Charles Rouillon <charles.rouillon@lp-digital.fr>
2015-02-20 14:24:44 +0100

    [Console] #11 - Add backbee console tool

--- Console/Console.php
+++ Console/Console.php
@@ -59,6 +59,7 @@ class Console extends ConsoleApplication
         parent::__construct('BackBee', BBApplication::VERSION.($application->isDebugMode() ? '/debug' : ''));
 
         $this->getDefinition()->addOption(new InputOption('--env', '-e', InputOption::VALUE_REQUIRED, 'The Environment name.', $this->_application->getEnvironment()));
+        $this->getDefinition()->addOption(new InputOption('--app', '-a', InputOption::VALUE_REQUIRED, 'The BackBee application classname.', get_class($this->_application)));
         $this->getDefinition()->addOption(new InputOption('--no-debug', null, InputOption::VALUE_NONE, 'Switches off debug mode.'));
     }
 
